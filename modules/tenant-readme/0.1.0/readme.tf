0215dc6598912129842294d95ed5253b3304403c
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-06-19 15:35:32 -0700

    chore: update glueops-platform version (#55)

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -37,7 +37,7 @@ locals {
   codespace_version         = "v0.24.0"
   argocd_crd_version        = "v2.7.4"
   argocd_helm_chart_version = "5.36.1"
-  glueops_platform_version  = "0.16.1"
+  glueops_platform_version  = "0.16.2"
   tools_version             = "v0.1.5"
 }
 
