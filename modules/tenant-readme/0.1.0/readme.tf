0f6d2feba3e8aba945034f5861cec1f227d2e32b
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-07-20 10:25:51 -0600

    feat: upgrade development-only-utilities (#64)

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -38,7 +38,7 @@ locals {
   argocd_crd_version        = "v2.7.8"
   argocd_helm_chart_version = "5.41.1"
   glueops_platform_version  = "0.23.1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
-  tools_version             = "v0.1.5"
+  tools_version             = "v0.1.6"
 }
 
 
