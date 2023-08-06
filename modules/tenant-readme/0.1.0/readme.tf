16afd5b594a417db09430d85ac6feb3496d5c700
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-06 08:37:30 -0700

    fix: the IAM role we create in dev environments. (#72)

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -38,7 +38,7 @@ locals {
   argocd_crd_version        = "v2.7.10"
   argocd_helm_chart_version = "5.42.1"
   glueops_platform_version  = "0.25.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
-  tools_version             = "v0.2.0"
+  tools_version             = "v0.3.0"
 }
 
 
