0b3e649035a415451fa2c34e8c3c166751aa45eb
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-10 20:47:19 -0700

    feat: bump glueops platform to v0.28.0 (#76)
    * feat: bump glueops platform to v0.28.0
    feat: adding support to specify nginx replicas count (default 2)
    feat: updating kube prometheus stack to 48.3.1
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- generate-helm-values.tf
+++ generate-helm-values.tf
@@ -37,7 +37,7 @@ resource "random_password" "grafana_admin_secret" {
 
 module "glueops_platform_helm_values" {
   for_each                                   = local.environment_map
-  source                                     = "git::https://github.com/GlueOps/platform-helm-chart-platform.git?ref=v0.27.1"
+  source                                     = "git::https://github.com/GlueOps/platform-helm-chart-platform.git?ref=v0.28.0"
   captain_repo_b64encoded_private_deploy_key = base64encode(module.captain_repository[each.value.environment_name].private_deploy_key)
   captain_repo_ssh_clone_url                 = module.captain_repository[each.value.environment_name].ssh_clone_url
   this_is_development                        = var.this_is_development
