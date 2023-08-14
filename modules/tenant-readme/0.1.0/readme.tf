220521da9dbd11a87d52785a8c72ca6bff220603
Venkata Mutyala <venkata@venkatamutyala.com>
2023-08-14 15:55:34 -0700

    feat: update platform version (#77)
    * feat: update platform version
    
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -42,7 +42,7 @@ locals {
   codespace_version         = "v0.28.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.42.2"
-  glueops_platform_version  = "0.28.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  glueops_platform_version  = "0.28.1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
   tools_version             = "v0.3.0"
 }
 
