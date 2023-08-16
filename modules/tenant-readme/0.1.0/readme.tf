22f31d69b816e372ea9f6dee677a7794f220c1c7
fernandoataoldotcom <antonio.rodriguez@glueops.dev>
2023-08-16 13:02:16 -0600

    chore: update tools 0.3.0 > 0.4.0 (#79)

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -43,7 +43,7 @@ locals {
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.42.2"
   glueops_platform_version  = "0.28.1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
-  tools_version             = "v0.3.0"
+  tools_version             = "v0.4.0"
 }
 
 
