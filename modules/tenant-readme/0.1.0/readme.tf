deb8d1b338105bfc3dbb6e23d867b6ead8fe92e1
Venkata Mutyala <venkata@venkatamutyala.com>
2023-09-05 21:13:18 -0700

    feat: update glueops-platform to v0.32.0 (#84)
    * feat: update glueops-platform to v0.32.0
    * chore(tools_version): v0.4.1 -> v0.4.2
    * terraform-docs: automated action
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: fernandoataoldotcom <antonio.rodriguez@glueops.dev>

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -42,8 +42,8 @@ locals {
   codespace_version         = "v0.29.0"
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.42.2"
-  glueops_platform_version  = "v0.30.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
-  tools_version             = "v0.4.1"
+  glueops_platform_version  = "v0.32.0" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
+  tools_version             = "v0.4.2"
 }
 
 
