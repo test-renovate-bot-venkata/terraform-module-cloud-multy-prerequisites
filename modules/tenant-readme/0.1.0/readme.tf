b993a1f8c6838d210c3e1aba67db81d9e6440978
Venkata Mutyala <venkata@venkatamutyala.com>
2024-01-30 07:54:00 -0800

    feat: updating docs for GCP (#123)
    * feat: updating docs for GCP
    * feat: update dev tools version

--- modules/tenant-readme/0.1.0/readme.tf
+++ modules/tenant-readme/0.1.0/readme.tf
@@ -43,7 +43,7 @@ locals {
   argocd_crd_version        = var.argocd_app_version
   argocd_helm_chart_version = "5.50.1"
   glueops_platform_version  = "v0.37.1" # this also needs to be updated in the module.glueops_platform_helm_values // generate-helm-values.tf
-  tools_version             = "v0.6.0"
+  tools_version             = "v0.7.0"
 }
 
 
